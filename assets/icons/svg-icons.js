8ef853afb76f6b46c03c45fe2bc67860e1b43124
Yann Gouffon <github@yago-art.ch>
2017-08-17 17:43:23 +0200

    💄 add base components

--- assets/icons/svg-icons.js
+++ assets/icons/svg-icons.js
@@ -6,7 +6,7 @@ const svgIcons = () => {
     var div = document.createElement('div');
     div.innerHTML = ajax.responseText;
     document.body.insertBefore(div, document.body.childNodes[0]);
-  }
+  };
 };
 
 export default svgIcons;
