c1cedd399065bbb02ad5f1610d7aa61bb713fc22
Yann Gouffon <github@yago-art.ch>
2017-12-06 13:53:34 +0100

    🍱 update  styleguide

--- assets/components/atoms/button/button.js
+++ assets/components/atoms/button/button.js
@@ -1,7 +1,10 @@
-import $ from 'jquery';
+// import $ from 'jquery';
+/* globals $ */
 
 export default () => {
-  $('.button').click(() => {
+  $('.button').click(function () {
+    console.log($(this));
+    console.log($(this).text());
     console.log('Strong is the Force inside you.');
   });
 };
--- assets/components/base.js
+++ assets/components/base.js
@@ -1,8 +1,12 @@
-import $ from 'jquery';
+// import $ from 'jquery';
+/* globals $ */
 
 import '../icons/svg-icons';
 import button from './atoms/button/button';
 
+console.log($);
+
 $(document).ready(() => {
+  console.log('ready !');
   button();
 });
